03ca581b4b21e674a970fb1c152c764cc2f2b92c
Geoffrey Gao <geoffreygao1@gmail.com>
2023-06-05 18:41:23 -0700

    Created components folder, updated file location references, installed redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import { configureStore } from 'redux';
+import { Provider } from 'react-redux';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
